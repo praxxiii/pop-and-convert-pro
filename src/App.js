e2779185c1aa413fb2edadc7208db4ddb82f6c80
Prakritee Sharma <prakritee.codewing@gmail.com>
2023-07-08 00:31:34 +0545

    icons component completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Welcome from './pages/Welcome';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,21 +1,22 @@
+import Icons from "../../components/Icons"
 import InfoCard from "./InfoCard"
 
 const cardContent =[
     {
         title: "Documentation",
         content: "Comprehensive and well-organized documentation that provides users with clear instructions and valuable information.",           
-        link: () => <a href="#" className="icon-link icon-link-hover">Explore Documentation<i className="icon bi-arrow-right"></i></a>
+        link: () => <a href="#" className="icon-link icon-link-hover">Explore Documentation <Icons icon="arrowRight" width={30} height={30}/></a>
     },
     {
         title: "Support",
         content: "Empower your customer support team with this practical video tutorial, designed to enhance their skills and knowledge.",
-        link: () => <a href="#" className="icon-link icon-link-hover">Contact Support Team<i className="icon bi-arrow-right"></i></a>
+        link: () => <a href="#" className="icon-link icon-link-hover">Contact Support Team<Icons icon="arrowRight" width={30} height={30}/></a>
 
     },
     {
         title: "Video Tutorials",
         content: "Unlock your creative potential with this comprehensive video tutorial that guides you step-by-step through mastering a new skill.",
-        link: () => <a href="#" className="icon-link icon-link-hover">Check Out Now<i className="icon bi-arrow-right"></i></a>
+        link: () => <a href="#" className="icon-link icon-link-hover">Check Out Now<Icons icon="arrowRight" width={30} height={30}/></a>
 
     }
 ]
