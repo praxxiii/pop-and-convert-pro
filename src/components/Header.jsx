f7d1e023cd18ad27371941e9a562eb1469ef810d
Prakritee Sharma <prakritee.codewing@gmail.com>
2023-07-19 17:56:26 +0545

    fix: added navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,18 @@
 import logo from '../assets/images/logo.png'
 import Icons from './Icons'
 
-export default function Header({changeScreen}) {
-
-    const handleScreenChange = (screen) => () => {
-        changeScreen(screen)
-    }
+export default function Header() {
 
     return <>
         <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
                 <img className="navbar-brand" src={logo}></img>
                 <div className="navbar-nav">
-                    <a href="#" className="nav-link" onClick={handleScreenChange('home')} aria-current="home" >Home</a>
-                    <a href="#" className="nav-link" onClick={handleScreenChange('list')} aria-current="list" >List</a>
-                    {/* <a className="nav-link" href="#"><Icons icon="globe" width={30} height={50} /></a>
+                    <a className="nav-link" href="#"><Icons icon="globe" width={30} height={50} /></a>
                     <a className="nav-link" href="#"><Icons icon="youtube" width={30} height={50} /></a>
                     <a className="nav-link" href="#"><Icons icon="headphone" width={30} height={50} /></a>
                     <a className="nav-link" href="#"><Icons icon="docs" width={30} height={50} /></a>
-                    <a className="nav-link" href="#"><Icons icon="announce" width={30} height={50} /></a> */}
+                    <a className="nav-link" href="#"><Icons icon="announce" width={30} height={50} /></a>
                 </div>
             </div>
         </nav>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router-dom"
-import Welcome from "./Welcome"
+import Header from "../components/Header.jsx"
 
 const Layout = () => {
     return <>
-        <Welcome/>
-        <Outlet/>
+        <Header />
+        <Outlet />
     </>
 }
 
